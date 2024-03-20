cc57376dcfdf8f64d1f6e2feaef675b7fc8c33e4
sebastian8456 <sebastian6358@gmail.com>
2024-03-19 21:42:27 -0500

    updated post requirements

--- forms.py
+++ forms.py
@@ -6,8 +6,8 @@ from flask_ckeditor import CKEditorField
 ##WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    subtitle = StringField("Subtitle", validators=[])
+    img_url = StringField("Blog Image URL", validators=[URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
